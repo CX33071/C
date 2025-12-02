0d927fb9dd53821e97711a12dc45e213cce6db92
CX33071 <3541053286@qq.com>
2025-12-02 11:24:44 +0800

    test

--- ls/ls5.c
+++ ls/ls5.c
@@ -60,9 +60,6 @@ int youxianji(char* str) {
     char* ptr = str;
     while (*ptr == '.') {
         ptr++;
-        if (*ptr == '\0') {
-            return 2;
-        }
     }
     unsigned char first_byte = (unsigned char)*ptr;
     if (first_byte >= 0xE0 && first_byte <= 0xEF) {
